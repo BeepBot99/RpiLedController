fa35b8fc71d94f3c0274bf9f841ec0499eb303ea
BeepBot99 <74377508+BeepBot99@users.noreply.github.com>
2025-01-22 19:44:24 -0600

    Add LED Page

--- ViewModels/LedViewModel.cs
+++ ViewModels/LedViewModel.cs
@@ -1,9 +1,30 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using RpiLedController.Models;
 
 namespace RpiLedController.ViewModels;
 
 internal partial class LedViewModel : ObservableObject
 {
+    private readonly Led _led;
+
     [ObservableProperty]
-    private string color = "Red";
+    private Color _color = Colors.DarkSlateGray;
+
+    [RelayCommand]
+    private void Toggle()
+    {
+        _led.IsOn = !_led.IsOn;
+        Color = _led.IsOn ? Colors.Yellow : Colors.DarkSlateGray;
+    }
+
+    public LedViewModel()
+    {
+        _led = new Led
+        {
+            Name = "My LED",
+            GpioPin = 18,
+            IsOn = false
+        };
+    }
 }
